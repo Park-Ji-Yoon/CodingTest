e05cc30103ebc00b2a195c63fdb8d40356b16d7e
Jiyoon Park <55547842+Park-Ji-Yoon@users.noreply.github.com>
2021-05-25 22:44:31 +0900

    백준 10809번:알파벳 찾기 문제 풀이 (python)

---
+++ baekjoon/10809_알파벳 찾기.py
@@ -0,0 +1,5 @@
+al = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+s = input()
+for a in al:
+  if a in s: print(s.index(a), end=" ")
+  else: print("-1", end=" ")
