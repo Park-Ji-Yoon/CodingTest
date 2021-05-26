149a2dc099ec13bdcebc5c8ca5f6324c8a990f2f
Jiyoon Park <55547842+Park-Ji-Yoon@users.noreply.github.com>
2021-05-27 05:17:28 +0900

    Update 1924_2007년.py

--- baekjoon/1924_2007년.py
+++ baekjoon/1924_2007년.py
@@ -1,1 +1,5 @@
-
+week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]  # 출력할 요일들을 일~월 순으로 초기화
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 월별 최대 일수 초기화 (1~12월)
+month, day = map(int, input().split())
+# month-1미만 인덱스까지의 months를 sum하고 day를 더한 값을 7로 나눈 나머지가 인덱스로 사용됨
+print(week[(sum(months[:month-1]) + day) % 7]) 
