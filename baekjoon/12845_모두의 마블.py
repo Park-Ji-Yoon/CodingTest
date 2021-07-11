cf6b7ceae2d9e4c0709a82081e3eaff5ce36728a
Jiyoon Park <55547842+Park-Ji-Yoon@users.noreply.github.com>
2021-07-11 21:49:02 +0900

    Create 12845_모두의 마블.py

---
+++ baekjoon/12845_모두의 마블.py
@@ -0,0 +1,10 @@
+num = int(input())
+arr = list((map(int, input().split())))
+print(arr)
+coin = 0
+
+max_level_v = max(arr)
+max_level_p = arr.index(max_level_v)
+
+while 
+  print(f'coin : {coin}')
